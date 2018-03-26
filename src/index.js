349b95a5e4859edbd756c369d76c098911e1ab89
Ivan <horbynko.i@gmail.com>
2018-03-26 23:33:34 +0300

    connected react-materialize

--- src/index.js
+++ src/index.js
@@ -2,17 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/index';
 import registerServiceWorker from './registerServiceWorker';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-ReactDOM.render(
-    <MuiThemeProvider>
-        <App/>
-    </MuiThemeProvider>
-    ,
-    document.getElementById('root')
-);
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
