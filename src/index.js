22071657d82fc369868e910d4f7a91c1f8aead1a
Ivan <horbynko.i@gmail.com>
2018-03-27 23:55:30 +0300

    added footer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from 'components/index';
 import registerServiceWorker from './registerServiceWorker';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import './css/style.css'
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
