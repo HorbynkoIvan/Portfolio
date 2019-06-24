80f148a88ddde11c7bcc2772a4bb9102bfcacb4f
Horbynko Ivan <horbynko.i@gmail.com>
2019-06-24 22:17:59 +0300

    update

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/index';
 import registerServiceWorker from './registerServiceWorker';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import './css/style.css'
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
+
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
